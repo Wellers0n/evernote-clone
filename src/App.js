f7ae8938320b3709353d5e9f47e19d6e92007f99
Wellerson <Wellerson.coffee@gmail.com>
2020-01-20 15:54:02 -0300

    added onclick's and onChange's

--- src/App.js
+++ src/App.js
@@ -25,10 +25,79 @@ function App() {
       });
   }, []);
 
+  const selectNote = (note, index) => {
+    console.log(note, index);
+    setSelectedNoteIndex(index);
+    setSelectedNote(note);
+  };
+
+  const noteUpdate = (id, noteObj) => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .doc(id)
+      .update({
+        title: noteObj.title,
+        body: noteObj.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+  };
+
+  const newNote = async title => {
+    const note = {
+      title: title,
+      body: ""
+    };
+    const newFromDB = await firebase
+      .firestore()
+      .collection("notes")
+      .add({ title: note.title, body: note.body, timestamp: firebase.firestore.FieldValue.serverTimestamp() });
+
+    const newId = newFromDB.id;
+    await setNotes([...notes, note]);
+    const newNoteIndex = notes.indexOf(notes.filter(_note => _note.id === newId)[0]);
+    setSelectedNote(notes[newNoteIndex]);
+    setSelectedNoteIndex(newNoteIndex);
+  };
+
+  const deleteNote = async note => {
+    const noteIndex = notes.indexOf(note);
+    await setNotes(notes.filter(_note => _note !== note))
+    if (selectedNoteIndex === noteIndex) {
+      setSelectedNote(null);
+      setSelectedNoteIndex(null);
+    } else {
+      if (notes.length > 1) {
+        selectNote(notes[selectedNoteIndex - 1], selectedNoteIndex - 1);
+      } else {
+        setSelectedNote(null);
+        setSelectedNoteIndex(null);
+      }
+    }
+    firebase
+      .firestore()
+      .collection("notes")
+      .doc(note.id)
+      .delete();
+  };
+
   return (
     <div>
-      <SidebarComponent notes={notes || []} selectedNoteIndex={selectedNoteIndex} />
-      <EditorComponent />
+      <SidebarComponent
+        deleteNote={deleteNote}
+        newNote={newNote}
+        selectNote={selectNote}
+        notes={notes || []}
+        selectedNoteIndex={selectedNoteIndex}
+      />
+      {selectedNote && (
+        <EditorComponent
+          noteUpdate={noteUpdate}
+          selectedNote={selectedNote}
+          selectedNoteIndex={selectedNoteIndex}
+          notes={notes || []}
+        />
+      )}
     </div>
   );
 }
