26c87ee7195a88d325a3819c6ef37ca8226d637b
Wellerson <Wellerson.coffee@gmail.com>
2020-01-20 03:27:29 -0300

    style and list itens

--- src/sidebaritem/sidebarItem.js
+++ src/sidebaritem/sidebarItem.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactQuill from "react-quill";
-import debouce from "../helpers";
-import borderColorIcon from "@material-ui/icons";
-import { withStyles } from "@material-ui/core";
+import { removeHTMLTags } from "../helpers";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { withStyles, ListItem, ListItemText } from "@material-ui/core";
 import styles from "./styles";
 
-const SidebarItemComponent = () => {
-    return (
-        <div>Hello from the sidebar item</div>
-    )
-}
+const SidebarItemComponent = props => {
+  const { _note, _index, selectedNoteIndex, selectedNote, classes } = props;
+  return (
+    <div key={_index}>
+      <ListItem className={classes.listItem} selected={selectedNoteIndex === _index} alignItems="flex-start">
+        <div className={classes.textSection} onCLick={() => selectedNote(_note, _index)}>
+            <ListItemText primary={_note.title} secondary={_note.body.substring(0, 30) + '...'}></ListItemText>
+        </div>
+      </ListItem>
+    </div>
+  );
+};
 
-export default withStyles(styles)(SidebarItemComponent)
+export default withStyles(styles)(SidebarItemComponent);
